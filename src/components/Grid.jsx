60d893f7f91c966dfed11ae194dded0e7bed567c
Kentin Even <kentineven02gmail.com>
2023-11-13 21:53:52 +0100

    add Grid to the project

---
+++ src/components/Grid.jsx
@@ -0,0 +1,22 @@
+const Grid = () => {
+  const grid = [
+    "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "", "",
+  ]
+
+  return (
+    <div className="p-4 flex flex-col gap-4 h-screen justify-center items-center">
+      <div className="flex flex-wrap w-64 box-content">
+        {grid.map(Array(64))}
+      </div>
+    </div>
+  )
+}
+
+export default Grid
