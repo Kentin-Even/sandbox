c84585a08e9780c139f5ddb931cc5c65a9494e5b
Kentin Even <kentineven02gmail.com>
2023-11-13 21:55:27 +0100

    add App

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import Grid from "./Grid"
+const App = () => {
+  // eslint-disable-next-line no-unused-vars
+  const grid = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]
+  ]
+
+  return (
+    <div className="text-center">
+      <Grid />
+    </div>
+  )
+}
+
+export default App
